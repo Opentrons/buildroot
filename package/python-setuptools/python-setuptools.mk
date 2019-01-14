be5a07ea6f14c813021589256b73647bbb41bf98
Thomas Petazzoni <thomas.petazzoni@bootlin.com>
2019-01-14 20:59:33 +0100

    package/python-setuptools: add comment about python3-setuptools
    As suggested by Arnout, add a comment to indicate that the version of
    python-setuptools must be kept in sync with python3-setuptools.
    
    Suggested-by: Arnout Vandecappelle <arnout@mind.be>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    
    (Opentrons cherry-picked from upstream commit 5d1988e)

--- package/python-setuptools/python-setuptools.mk
+++ package/python-setuptools/python-setuptools.mk
@@ -4,6 +4,8 @@
 #
 ################################################################################
 
+# Please keep in sync with
+# package/python3-setuptools/python3-setuptools.mk
 PYTHON_SETUPTOOLS_VERSION = 40.5.0
 PYTHON_SETUPTOOLS_SOURCE = setuptools-$(PYTHON_SETUPTOOLS_VERSION).zip
 PYTHON_SETUPTOOLS_SITE = https://files.pythonhosted.org/packages/26/e5/9897eee1100b166a61f91b68528cb692e8887300d9cbdaa1a349f6304b79
