2810fb4145adbab6c5139fd8f7dc9833d09ad69d
Seth Foster <seth@opentrons.com>
2019-02-11 16:18:35 -0500

    chore(build): Split rootfs into root and var partitions
    Since we want to update entire partitions while keeping some user data, we have to split the filesystem apart into multiple locations.
    
    One partition (the "home" partition) is mounted at /var. /var/home is bind mounted to /home and /var/data is bind mounted to /data. This is in the last partition, which will be autoexpanded, and will never change - this is the persistent fs.
    
    The root filesystem, which has everything that isn't in /var, /home, or /data, is determined by the kernel command line and is one of either of the root partitions. These are the ones that will be overwritten in updates, and they are mounted readonly.
    
    Right now, this is all accomplished by
    
    - Adding a /var image to the genimage config
    - Removing /var from the staging fakeroot environment it's put into the root filesystem image
    - Modifying /etc/fstab in post-build.sh
    
    This all may change as things get more complex.
    
    Closes #2866

---
+++ board/opentrons/ot2/in-fakeroot.sh
@@ -0,0 +1,3 @@
+# Remove /var from rootfs because it will be in its own partition
+set -ev
+rm -rf ${TARGET_DIR}/var
--- board/opentrons/ot2/post-build.sh
+++ board/opentrons/ot2/post-build.sh
@@ -9,3 +9,10 @@ if [ -e ${TARGET_DIR}/etc/inittab ]; then
 	sed -i '/GENERIC_SERIAL/a\
 tty1::respawn:/sbin/getty -L  tty1 0 vt100 # HDMI console' ${TARGET_DIR}/etc/inittab
 fi
+
+cat <<EOF > ${TARGET_DIR}/etc/fstab
+/dev/root / auto ro 0 1
+/dev/mmcblk0p4 /var auto rw 0 2
+/var/data /data none defaults,bind 0 0
+/var/home /home none defaults,bind 0 0
+EOF
