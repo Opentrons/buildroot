bc7764258155ff24caa56182fb64dbd4850698d4
Seth Foster <sfoster1@users.noreply.github.com>
2019-02-15 12:57:27 -0500

    feat(boot): Boot from one of two different root partitions (#5)
    We want to have two parallel root filesystems (containing everything needed for
    boot), so that you can boot from one, write to the other, reboot into the other,
    and fallback if it fails.
    
    To accomplish this, we need to be able to select where we boot at boot-time
    itself. This is not something the raspberry pi bootloaders can do. So, we add
    u-boot. U-boot has support for the raspberry pi and integrates nicely with the
    raspberry pi firmware and bootloaders themselves.
    
    The boot process now becomes
    
    broadcom internal
            |
    raspi start.elf sequence with gpu
            |
    raspi boot sees kernel=u-boot.bin in config, boots u-boot on arm cpu with
    device tree and command line args for linux
            |
    u-boot comes up, inherits the compiled device tree + overlays from start.elf,
    does distro_boot process and sees our boot.scr
            |
    boot.scr reads from the uboot environment to figure out which partition to boot
    and does so. it also knows how to switch which partition to boot if the boot
    fails.
    
    This has a couple upshots, like
    - There’s an extra bootloader now
    - Even though there’s an extra bootloader now, most of the kernel options are
    controlled in the rpi way still, by changing cmdline.txt - uboot just intercepts
    and changes the boot partition
    
    Once you’re in the system, we now install uboot-tools, which lets you modify
    uboot environment variables. There’s also a script called ot-switch-partitions,
    which mounts the boot part, flips which root part should be booted, and unmounts
    the boot part again.
    
    Closes https://github.com/Opentrons/opentrons/issues/2868

--- board/opentrons/ot2/post-build.sh
+++ board/opentrons/ot2/post-build.sh
@@ -10,9 +10,22 @@ if [ -e ${TARGET_DIR}/etc/inittab ]; then
 tty1::respawn:/sbin/getty -L  tty1 0 vt100 # HDMI console' ${TARGET_DIR}/etc/inittab
 fi
 
+# Write an fstab that will do our /var and bind mounts
 cat <<EOF > ${TARGET_DIR}/etc/fstab
 /dev/root / auto ro 0 1
 /dev/mmcblk0p4 /var auto rw 0 2
 /var/data /data none defaults,bind 0 0
 /var/home /home none defaults,bind 0 0
+/var/mnt /mnt none defaults,bind 0 0
 EOF
+
+# Get our kernel and dt(s) in rootfs
+mkdir -p ${TARGET_DIR}/boot
+cp ${BINARIES_DIR}/zImage ${TARGET_DIR}/boot/zImage
+mkdir -p ${TARGET_DIR}/boot/overlays
+mkdir -p ${TARGET_DIR}/mnt/bootpart
+cp -r ${BINARIES_DIR}/rpi-firmware/overlays ${TARGET_DIR}/boot/overlays
+cp -r ${BINARIES_DIR}/*.dtb ${TARGET_DIR}/boot/
+
+# rewrite config.txt to boot u-boot
+sed -i s/kernel=zImage/kernel=u-boot.bin/ ${BINARIES_DIR}/rpi-firmware/config.txt
