ffa34c7466874343b5f660d45b23049e9790f292
Seth Foster <seth@opentrons.com>
2019-03-07 22:24:48 +0000

    feat(system): Include opentrons api server from external tree
    - Adds a BR2_EXTERNAL tree from the
      monorepo (https://github.com/Opentrons/opentrons/pull/3217) and pulls in the opentrons api server to the build.
    
    - Fixes some issues in systemd services and targets. Codebuild rewrites
      broken (and maybe non-broken) absolute links to fit inside its chroot jail,
      which was breaking the absolute links that aren’t supposed to be valid until
      they’re on the robot
    
    - Add a VERSIONS file taken from a combination of the buildroot version (from
      git) and opentrons api server versions (from package.json) and save to
      /etc/VERSIONS.json
    
    - VERSIONS.json is a new artifact and also zipped up with the full images and
      update images when uploaded to s3
    
    Closes https://github.com/Opentrons/opentrons/issues/2877

---
+++ board/opentrons/ot2/write_version.py
@@ -0,0 +1,56 @@
+import json
+import os
+import sys
+import subprocess
+import argparse
+
+try:
+    br_version = subprocess.check_output(
+        ['git', 'describe', '--tags', '--always'],
+        stderr=subprocess.STDOUT).decode().strip()
+except subprocess.CalledProcessError as cpe:
+    print("{}: {}: {}".format(cpe.cmd, cpe.returncode, cpe.stdout))
+    print("Defaulting to (unknown)")
+    br_version = 'unknown'
+
+try:
+    br_sha = subprocess.check_output(
+        ['git', 'rev-parse', 'HEAD'],
+        stderr=subprocess.STDOUT).decode().strip()
+except subprocess.CalledProcessError as cpe:
+    print("{}: {}: {}".format(cpe.cmd, cpe.returncode, cpe.stdout))
+    print("Defaulting to (unknown)")
+    br_sha = 'unknown'
+
+try:
+    br_branch_from_git = subprocess.check_output(
+        ['git', 'rev-parse', '--abbrev-ref', 'HEAD'],
+        stderr=subprocess.STDOUT).decode().strip()
+except subprocess.CalledProcessError as cpe:
+    print("{}: {}: {}".format(cpe.cmd, cpe.returncode, cpe.stdout))
+    print("Defaulting to (unknown)")
+    br_branch = 'unknown'
+
+br_branch = os.getenv('CODEBUILD_SOURCE_VERSION', br_branch_from_git)
+build_id = os.getenv('CODEBUILD_BUILD_ID', 'dev')
+
+parser = argparse.ArgumentParser(description='Write a version file to buildroot')
+parser.add_argument('in_versions', metavar='IN_VERSIONS',
+                    type=argparse.FileType('r'), nargs='+',
+                    help='A list of files to read versions from. These should '
+                         'be json files with non-overlapping keys that will be'
+                         ' unified (along with the buildroot version from git)'
+                         ' into the final product.')
+parser.add_argument('outfile', metavar='OUT', type=argparse.FileType('w'),
+                    help='The file to write to')
+args = parser.parse_args()
+
+version_dict = {'buildroot_version': br_version,
+                'buildroot_sha': br_sha,
+                'buildroot_branch': br_branch,
+                'buildroot_buildid': build_id}
+
+for f in args.in_versions:
+    version_dict.update(json.load(f))
+
+json.dump(version_dict, args.outfile)
