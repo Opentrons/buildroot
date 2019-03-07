49fbe51f6301046e1d7125ac0ee6d389456035bd
Seth Foster <seth@opentrons.com>
2019-03-07 22:24:48 +0000

    feat(system): Include opentrons api server from external tree

--- opentrons-build.sh
+++ opentrons-build.sh
@@ -19,7 +19,11 @@
 # are provided, it will run the docker container once with the arguments
 set -e -x -o pipefail
 
-DOCKER_BIND="--mount type=bind,source=$(pwd),destination=/buildroot"
+DOCKER_BR_BIND_DIR="/buildroot"
+DOCKER_OT_BIND_DIR="/opentrons"
+DOCKER_BIND_BR="--mount type=bind,source=$(pwd),destination=${DOCKER_BR_BIND_DIR}"
+DOCKER_BIND_OT="--mount type=bind,source=$(pwd)/../opentrons,destination=${DOCKER_OT_BIND_DIR}"
+DOCKER_BIND="${DOCKER_BIND_BR} ${DOCKER_BIND_OT}"
 heads=${@:1:$(($# - 1))}
 tail=${@:$#}
 
