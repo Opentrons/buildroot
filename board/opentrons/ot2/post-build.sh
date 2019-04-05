2ff52924680b0173304ba94f1d3c0ecfb4e5e80e
Seth Foster <seth@opentrons.com>
2019-04-05 17:25:59 -0400

    feat(build): Support balena migration
    - Change our image and fstab to exactly match the one balena uses, so we don't have to rewrite the partition table during migration
    - Add an ot2-migration.zip artifact, which contains rootfs.ext4, boot.vfat, and hashes for same
    - Make sure nginx allows large file transfers
    - Bind-mount hostname files from /var so we can set our name
    - Run avahi-daemon later than usual during boot so it actually picks up the changed hostname

--- board/opentrons/ot2/post-build.sh
+++ board/opentrons/ot2/post-build.sh
@@ -18,10 +18,12 @@ fi
 # Write an fstab that will do our /var and bind mounts
 cat <<EOF > ${TARGET_DIR}/etc/fstab
 /dev/root / auto ro 0 0
-/dev/mmcblk0p4 /var auto rw,x-systemd.growfs 0 2
+/dev/mmcblk0p6 /var auto rw,x-systemd.growfs 0 2
 /var/data /data none defaults,bind 0 0
 /var/home /root none defaults,bind 0 0
 /var/mnt /mnt none defaults,bind 0 0
+/var/hostname /etc/hostname none defaults,bind,nofail 0 0
+/var/machine-info /etc/machine-info none defaults,bind,nofail 0 0
 EOF
 
 # Get our kernel and dt(s) in rootfs
@@ -34,6 +36,7 @@ cp -r ${BINARIES_DIR}/*.dtb ${TARGET_DIR}/boot/
 
 # rewrite config.txt to boot u-boot
 sed -i s/kernel=zImage/kernel=u-boot.bin/ ${BINARIES_DIR}/rpi-firmware/config.txt
+hostname_to_write=$(cat ${TARGET_DIR}/var/hostname)
 
 if [ ${OT_BUILD_TYPE} != "release" ]; then
     echo "Build type is NOT RELEASE, adding default ssh key and removing signing"
@@ -41,8 +44,23 @@ if [ ${OT_BUILD_TYPE} != "release" ]; then
     cat ${TARGET_DIR}/var/home/.ssh/robot_key.pub > ${TARGET_DIR}/var/home/.ssh/authorized_keys
     # remove code signing cert (allows unsigned updates)
     rm ${TARGET_DIR}/etc/opentrons-robot-signing-key.crt
+    deployment_to_write="development"
+
 else
     echo "Build type is RELEASE"
+    deployment_to_write="production"
+fi
+
+
+cat <<EOF >> "${TARGET_DIR}/etc/machine-info"
+PRETTY_HOSTNAME=${hostname_to_write}
+DEPLOYMENT=${deployment_to_write}
+EOF
+
+# Start avahi-daemon as late as possible so it catches hostnamed changing
+# the hostname
+if [[ -f ${TARGET_DIR}/etc/systemd/system/multi-user.target.wants/avahi-daemon.service ]]; then
+    mv ${TARGET_DIR}/etc/systemd/system/multi-user.target.wants/avahi-daemon.service ${TARGET_DIR}/etc/systemd/system/opentrons.target.wants/avahi-daemon.service
 fi
 
 python ./board/opentrons/ot2/write_version.py ${BINARIES_DIR}/opentrons-api-version.json ${BINARIES_DIR}/opentrons-update-server-version.json ${BINARIES_DIR}/VERSION.json
