5054932b460c3b275d2ef529244a17c1c1e8e9fb
Seth Foster <seth@opentrons.com>
2019-05-10 14:01:06 -0400

    feat(system): Allow users to install things with pip
    Some users, when running complex protocols, need python packages that aren't
    present in the buildroot config. For them, we need to allow pip. This commit
    - Installs pip
    - Configures pip to install to /var/user-packages as a prefix, meaning that any
    python packages end up in /var/user-packages/usr/lib/python3.7/site-packages,
    any scripts end up in /var/user-packages/usr/bin/, etc
    - Configures python to look in
    /var/user-packages/usr/lib/python3.7/site-packages for packages
    - Adds /var/user-packages/usr/bin/ to the path
    
    Note that until you actually install something with pip, /var/user-packages
    won't exist and therefore if you do `python -c "import sys; print(sys.path)" it
    won't show the installed path.
    
    Closes https://github.com/Opentrons/opentrons/issues/3358

--- board/opentrons/ot2/rootfs-overlay/etc/profile.d/ot-environ.sh
+++ board/opentrons/ot2/rootfs-overlay/etc/profile.d/ot-environ.sh
@@ -1,2 +1,5 @@
+# Make sure that users of the api know this is a smoothie
 export RUNNING_ON_PI=1
 export OT_SMOOTHIE_ID=AMA
+# Make sure scripts installed by pip are in the path
+export PATH=$PATH:/var/user-packages/usr/bin
