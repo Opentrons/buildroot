fd7fb622338ff7f844c3b387f43ebd30629d3d16
Seth Foster <seth@opentrons.com>
2019-10-02 16:36:30 -0400

    chore(config) Update jsonschema (#43)
    * fix(package/jsonschema): bump version to 3.0.2
    
    Avoids a bug involving validating enum elements that involve unicode on windows.
    
    * configs/ot2_defconfig: add python-pyrsistent
    
    Required by updating jsonschema

--- package/python-jsonschema/python-jsonschema.mk
+++ package/python-jsonschema/python-jsonschema.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_JSONSCHEMA_VERSION = 2.5.1
+PYTHON_JSONSCHEMA_VERSION = 3.0.2
 PYTHON_JSONSCHEMA_SOURCE = jsonschema-$(PYTHON_JSONSCHEMA_VERSION).tar.gz
-PYTHON_JSONSCHEMA_SITE = https://pypi.python.org/packages/58/0d/c816f5ea5adaf1293a1d81d32e4cdfdaf8496973aa5049786d7fdb14e7e7
+PYTHON_JSONSCHEMA_SITE = https://files.pythonhosted.org/packages/92/d2/8768983d6e4bb1478ea08e7bc186304f47b99c22bfd711caa8e9becb0361
 PYTHON_JSONSCHEMA_SETUP_TYPE = setuptools
 PYTHON_JSONSCHEMA_LICENSE = MIT
 PYTHON_JSONSCHEMA_LICENSE_FILES = COPYING json/LICENSE
