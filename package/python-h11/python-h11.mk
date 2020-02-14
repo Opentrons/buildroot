c038bd5fed0574f409283f15cf538ab897d7cc42
amit lissack <amit@opentrons.com>
2020-02-14 14:52:32 +0000

    package/python-h11: new package

--- package/python-h11/python-h11.mk
+++ package/python-h11/python-h11.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_H11_VERSION = 0.14.0
+PYTHON_H11_VERSION = 0.9.0
 PYTHON_H11_SOURCE = h11-$(PYTHON_H11_VERSION).tar.gz
-PYTHON_H11_SITE = https://files.pythonhosted.org/packages/f5/38/3af3d3633a34a3316095b39c8e8fb4853a28a536e55d347bd8d8e9a14b03
+PYTHON_H11_SITE = https://files.pythonhosted.org/packages/34/5a/abaa557d20b210117d8c3e6b0b817ce9b329b2e81f87612e60102a924323
 PYTHON_H11_SETUP_TYPE = setuptools
 PYTHON_H11_LICENSE = MIT
 PYTHON_H11_LICENSE_FILES = LICENSE.txt
