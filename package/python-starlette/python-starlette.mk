3777ae0d3046131849564dd44ed72fe7308bfe98
amit lissack <amit@opentrons.com>
2020-03-03 13:19:12 +0000

    package/python-starlette: downgrade to version 0.12.9 to match requirement of fastapi 0.48.0

--- package/python-starlette/python-starlette.mk
+++ package/python-starlette/python-starlette.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_STARLETTE_VERSION = 0.13.1
+PYTHON_STARLETTE_VERSION = 0.12.9
 PYTHON_STARLETTE_SOURCE = starlette-$(PYTHON_STARLETTE_VERSION).tar.gz
-PYTHON_STARLETTE_SITE = https://files.pythonhosted.org/packages/2c/e4/244978c726428c440a333d0cbf68913eb0816a565822fdce3075d1c729f1
+PYTHON_STARLETTE_SITE = https://files.pythonhosted.org/packages/67/95/2220fe5bf287e693a6430d8ee36c681b0157035b7249ec08f8fb36319d16
 PYTHON_STARLETTE_SETUP_TYPE = setuptools
 PYTHON_STARLETTE_LICENSE = BSD-3-Clause
 
