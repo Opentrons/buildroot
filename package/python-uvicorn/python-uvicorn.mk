25d566897a0a630175b8fa387904749b111b0561
amit lissack <amit@opentrons.com>
2020-03-30 17:13:50 +0000

    package/python-uvicorn: upgrade to 0.11.3 from 0.11.2

--- package/python-uvicorn/python-uvicorn.mk
+++ package/python-uvicorn/python-uvicorn.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_UVICORN_VERSION = 0.11.2
+PYTHON_UVICORN_VERSION = 0.11.3
 PYTHON_UVICORN_SOURCE = uvicorn-$(PYTHON_UVICORN_VERSION).tar.gz
-PYTHON_UVICORN_SITE = https://files.pythonhosted.org/packages/5c/ed/37f168f52814d09feaf0b4f6894704a17209f84219773def5b8b14a060dc
+PYTHON_UVICORN_SITE = https://files.pythonhosted.org/packages/48/30/cd48ac0eb03bd3007191ea20fc34457a45187fb430fdc30c0f6b9059bee0
 PYTHON_UVICORN_SETUP_TYPE = setuptools
 
 $(eval $(python-package))
