cb6ee50df75c676a5c5c3b1c62fe8be7ff25956f
amit lissack <amit@opentrons.com>
2020-04-16 17:02:53 +0000

    package/python-fastapi: upgrade to 0.54.1

--- package/python-fastapi/python-fastapi.mk
+++ package/python-fastapi/python-fastapi.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_FASTAPI_VERSION = 0.48.0
+PYTHON_FASTAPI_VERSION = 0.54.1
 PYTHON_FASTAPI_SOURCE = fastapi-$(PYTHON_FASTAPI_VERSION).tar.gz
-PYTHON_FASTAPI_SITE = https://files.pythonhosted.org/packages/37/d9/679462f5d48458afc8afcc3c8cc62177843aaafb416162666c3fa7b162f3
+PYTHON_FASTAPI_SITE = https://files.pythonhosted.org/packages/4d/54/45770888a4a73666390120b444131568637e70b5e002860bad99fa1dcb27
 PYTHON_FASTAPI_SETUP_TYPE = distutils
 PYTHON_FASTAPI_LICENSE = MIT
 PYTHON_FASTAPI_LICENSE_FILES = LICENSE
