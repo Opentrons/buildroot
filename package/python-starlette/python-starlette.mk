628d24702f04d00c083eeba040ba87a1dd22cf18
amit lissack <amit@opentrons.com>
2020-04-16 17:02:12 +0000

    package/python-starlette: upgrade to 0.13.2

--- package/python-starlette/python-starlette.mk
+++ package/python-starlette/python-starlette.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_STARLETTE_VERSION = 0.12.9
+PYTHON_STARLETTE_VERSION = 0.13.2
 PYTHON_STARLETTE_SOURCE = starlette-$(PYTHON_STARLETTE_VERSION).tar.gz
-PYTHON_STARLETTE_SITE = https://files.pythonhosted.org/packages/67/95/2220fe5bf287e693a6430d8ee36c681b0157035b7249ec08f8fb36319d16
+PYTHON_STARLETTE_SITE = https://files.pythonhosted.org/packages/8c/2e/78248ccba2102bc1100ed5cad26d91e03806f4f04ff47cccb412d2bb2855
 PYTHON_STARLETTE_SETUP_TYPE = setuptools
 PYTHON_STARLETTE_LICENSE = BSD-3-Clause
 
