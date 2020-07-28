beba95539ab094cf27f7a9342a936daa92b262d1
amit lissack <amit@opentrons.com>
2020-07-28 15:57:13 -0400

    package/python-wsproto: new package

--- package/python-wsproto/python-wsproto.mk
+++ package/python-wsproto/python-wsproto.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_WSPROTO_VERSION = 1.2.0
+PYTHON_WSPROTO_VERSION = 0.15.0
 PYTHON_WSPROTO_SOURCE = wsproto-$(PYTHON_WSPROTO_VERSION).tar.gz
-PYTHON_WSPROTO_SITE = https://files.pythonhosted.org/packages/c9/4a/44d3c295350d776427904d73c189e10aeae66d7f555bb2feee16d1e4ba5a
+PYTHON_WSPROTO_SITE = https://files.pythonhosted.org/packages/7b/5c/7b125c14fbdbeb7913d61e5841e20a453d71d5a5d961354a384c3ceeb81c
 PYTHON_WSPROTO_SETUP_TYPE = setuptools
 PYTHON_WSPROTO_LICENSE = MIT
 PYTHON_WSPROTO_LICENSE_FILES = LICENSE
