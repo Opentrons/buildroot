33fc05eaf7de33028630eb5a249086ee4b995977
Mike Cousins <mike@opentrons.com>
2021-08-23 19:21:09 -0400

    package/python-dotenv: bump to version 0.19.0

--- package/python-dotenv/python-dotenv.mk
+++ package/python-dotenv/python-dotenv.mk
@@ -4,11 +4,11 @@
 #
 ################################################################################
 
-PYTHON_DOTENV_VERSION = 0.12.0
+PYTHON_DOTENV_VERSION = 0.19.0
 PYTHON_DOTENV_SOURCE = python-dotenv-$(PYTHON_DOTENV_VERSION).tar.gz
-PYTHON_DOTENV_SITE = https://files.pythonhosted.org/packages/81/8b/26f807fa58582455f8ebb07f1b7473ee1a21b4671d1eaf4fecece9337e2a
+PYTHON_DOTENV_SITE = https://files.pythonhosted.org/packages/59/39/20eb771fc2113fb67638d4f2e1905c51b0c75862d09018a393470234a51c
 PYTHON_DOTENV_SETUP_TYPE = setuptools
-PYTHON_DOTENV_LICENSE = LICENSE.txt
+PYTHON_DOTENV_LICENSE = BSD-3-Clause
 PYTHON_DOTENV_LICENSE_FILES = LICENSE
 
 $(eval $(python-package))
