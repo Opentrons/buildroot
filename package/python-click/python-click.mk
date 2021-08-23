ae31061c5f0a259b26c404906872f09ec6ed6798
Mike Cousins <mike@opentrons.com>
2021-08-23 19:22:17 -0400

    package/python-click: bump to version 8.0.1

--- package/python-click/python-click.mk
+++ package/python-click/python-click.mk
@@ -4,11 +4,11 @@
 #
 ################################################################################
 
-PYTHON_CLICK_VERSION = 7.0
-PYTHON_CLICK_SOURCE = Click-$(PYTHON_CLICK_VERSION).tar.gz
-PYTHON_CLICK_SITE = https://files.pythonhosted.org/packages/f8/5c/f60e9d8a1e77005f664b76ff8aeaee5bc05d0a91798afd7f53fc998dbc47
+PYTHON_CLICK_VERSION = 8.0.1
+PYTHON_CLICK_SOURCE = click-$(PYTHON_CLICK_VERSION).tar.gz
+PYTHON_CLICK_SITE = https://files.pythonhosted.org/packages/21/83/308a74ca1104fe1e3197d31693a7a2db67c2d4e668f20f43a2fca491f9f7
+PYTHON_CLICK_SETUP_TYPE = setuptools
 PYTHON_CLICK_LICENSE = BSD-3-Clause
 PYTHON_CLICK_LICENSE_FILES = LICENSE.rst
-PYTHON_CLICK_SETUP_TYPE = setuptools
 
 $(eval $(python-package))
