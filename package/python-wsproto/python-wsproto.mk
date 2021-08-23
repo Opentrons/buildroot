95323715664f7f2192105c9bc588d6100be3b3cf
Mike Cousins <mike@opentrons.com>
2021-08-23 19:32:16 -0400

    package/python-wsproto: bump to version 1.0.0

--- package/python-wsproto/python-wsproto.mk
+++ package/python-wsproto/python-wsproto.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_WSPROTO_VERSION = 0.15.0
+PYTHON_WSPROTO_VERSION = 1.0.0
 PYTHON_WSPROTO_SOURCE = wsproto-$(PYTHON_WSPROTO_VERSION).tar.gz
-PYTHON_WSPROTO_SITE = https://files.pythonhosted.org/packages/7b/5c/7b125c14fbdbeb7913d61e5841e20a453d71d5a5d961354a384c3ceeb81c
+PYTHON_WSPROTO_SITE = https://files.pythonhosted.org/packages/2b/a4/aded0882f8f1cddd68dcd531309a15bf976f301e6a3554055cc06213c227
 PYTHON_WSPROTO_SETUP_TYPE = setuptools
 PYTHON_WSPROTO_LICENSE = MIT
 PYTHON_WSPROTO_LICENSE_FILES = LICENSE
