8c993cad51e722fd183c121f1f8a72f58b04088a
Mike Cousins <mike@opentrons.com>
2021-08-23 19:37:43 -0400

    package/python-uvicorn: bump to version 0.14.0

--- package/python-uvicorn/python-uvicorn.mk
+++ package/python-uvicorn/python-uvicorn.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_UVICORN_VERSION = 0.11.3
+PYTHON_UVICORN_VERSION = 0.14.0
 PYTHON_UVICORN_SOURCE = uvicorn-$(PYTHON_UVICORN_VERSION).tar.gz
-PYTHON_UVICORN_SITE = https://files.pythonhosted.org/packages/48/30/cd48ac0eb03bd3007191ea20fc34457a45187fb430fdc30c0f6b9059bee0
+PYTHON_UVICORN_SITE = https://files.pythonhosted.org/packages/e9/9e/25d59f5043cf763833b2581c8027fa92342c4cf8ee523b498ecdf460c16d
 PYTHON_UVICORN_SETUP_TYPE = setuptools
 
 $(eval $(python-package))
