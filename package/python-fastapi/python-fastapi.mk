006a1ff442d2b1b029735213bdc3c4face05a933
Mike Cousins <mike@opentrons.com>
2021-09-15 20:29:52 +0000

    package/python-fastapi: bump to version 0.68.1

--- package/python-fastapi/python-fastapi.mk
+++ package/python-fastapi/python-fastapi.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_FASTAPI_VERSION = 0.54.1
+PYTHON_FASTAPI_VERSION = 0.68.1
 PYTHON_FASTAPI_SOURCE = fastapi-$(PYTHON_FASTAPI_VERSION).tar.gz
-PYTHON_FASTAPI_SITE = https://files.pythonhosted.org/packages/4d/54/45770888a4a73666390120b444131568637e70b5e002860bad99fa1dcb27
+PYTHON_FASTAPI_SITE = https://files.pythonhosted.org/packages/e0/2a/03c3e1ede3aa83c45da1d156b0180288e573f5e2643ec4fd6d697cebebdf
 PYTHON_FASTAPI_SETUP_TYPE = distutils
 PYTHON_FASTAPI_LICENSE = MIT
 PYTHON_FASTAPI_LICENSE_FILES = LICENSE
