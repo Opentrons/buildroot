5b5bdbadd5ac1b86e4482a2d82bbeebf734991af
Mike Cousins <mike@opentrons.com>
2021-09-15 20:30:07 +0000

    package/python-starlette: bump to version 0.14.2

--- package/python-starlette/python-starlette.mk
+++ package/python-starlette/python-starlette.mk
@@ -4,10 +4,11 @@
 #
 ################################################################################
 
-PYTHON_STARLETTE_VERSION = 0.13.2
+PYTHON_STARLETTE_VERSION = 0.14.2
 PYTHON_STARLETTE_SOURCE = starlette-$(PYTHON_STARLETTE_VERSION).tar.gz
-PYTHON_STARLETTE_SITE = https://files.pythonhosted.org/packages/8c/2e/78248ccba2102bc1100ed5cad26d91e03806f4f04ff47cccb412d2bb2855
+PYTHON_STARLETTE_SITE = https://files.pythonhosted.org/packages/3a/06/ded663a1ddea8b11a2027d88ff0e757f9cdb812310f18bee33ef7270112f
 PYTHON_STARLETTE_SETUP_TYPE = setuptools
 PYTHON_STARLETTE_LICENSE = BSD-3-Clause
+PYTHON_STARLETTE_LICENSE_FILES = LICENSE.md
 
 $(eval $(python-package))
