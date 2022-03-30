6d5a1cefcc5f8114227370f001d951413ff946e4
Mike Cousins <mike@opentrons.com>
2022-03-30 10:49:46 -0400

    package/python-sqlalchemy: bump to version 1.4.32

--- package/python-sqlalchemy/python-sqlalchemy.mk
+++ package/python-sqlalchemy/python-sqlalchemy.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_SQLALCHEMY_VERSION = 1.4.42
+PYTHON_SQLALCHEMY_VERSION = 1.4.32
 PYTHON_SQLALCHEMY_SOURCE = SQLAlchemy-$(PYTHON_SQLALCHEMY_VERSION).tar.gz
-PYTHON_SQLALCHEMY_SITE = https://files.pythonhosted.org/packages/e4/56/8ea85eaab7d93b58f9c213ad8fc5882838189a29fc8cc401d80710a12969
+PYTHON_SQLALCHEMY_SITE = https://files.pythonhosted.org/packages/7a/9f/ace7376a3ab45adf0f7169a5d8d60707c04b171b72a18bb23d505f83f362
 PYTHON_SQLALCHEMY_SETUP_TYPE = setuptools
 PYTHON_SQLALCHEMY_LICENSE = MIT
 PYTHON_SQLALCHEMY_LICENSE_FILES = LICENSE
