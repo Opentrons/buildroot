695fd7bd92fa0594ed71b02a9e706fa5b33e64eb
Frank Sinapi <frank.sinapi@opentrons.com>
2022-08-19 15:03:22 -0400

    Updated Heater Shaker firmware version and SHA

--- package/opentrons/opentrons-heater-shaker-firmware/opentrons-heater-shaker-firmware.mk
+++ package/opentrons/opentrons-heater-shaker-firmware/opentrons-heater-shaker-firmware.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-OPENTRONS_HEATER_SHAKER_FIRMWARE_VERSION=v1.0.1
-OPENTRONS_HEATER_SHAKER_FIRMWARE_SOURCE=heater-shaker@v1.0.1.bin
+OPENTRONS_HEATER_SHAKER_FIRMWARE_VERSION=v1.0.2
+OPENTRONS_HEATER_SHAKER_FIRMWARE_SOURCE=heater-shaker@v1.0.2.bin
 OPENTRONS_HEATER_SHAKER_FIRMWARE_SITE=https://opentrons-modules-builds.s3.us-east-2.amazonaws.com/heater-shaker/$(OPENTRONS_HEATER_SHAKER_FIRMWARE_VERSION)
 OPENTRONS_HEATER_SHAKER_FIRMWARE_LICENSE=Apache-2.0
 
