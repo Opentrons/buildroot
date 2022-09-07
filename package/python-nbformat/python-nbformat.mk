7c3090b968a1fb1b33d44d67a3e87415e55d0e38
Seth Foster <seth@opentrons.com>
2022-09-07 14:24:47 -0400

    package/python-nbformat: remove tests

--- package/python-nbformat/python-nbformat.mk
+++ package/python-nbformat/python-nbformat.mk
@@ -10,4 +10,11 @@ PYTHON_NBFORMAT_SITE = https://files.pythonhosted.org/packages/6e/0e/160754f7ae3
 PYTHON_NBFORMAT_SETUP_TYPE = distutils
 PYTHON_NBFORMAT_LICENSE = BSD-3-Clause
 
+ifneq ($(BR2_PACKAGE_PYTHON_NBFORMAT_TESTS),y)
+define PYTHON_NBFORMAT_REMOVE_TESTS
+   rm -rf $(TARGET_DIR)/usr/lib/python*/site-packages/nbformat/tests
+endef
+PYTHON_NBFORMAT_POST_INSTALL_TARGET_HOOKS += PYTHON_NBFORMAT_REMOVE_TESTS
+endif
+
 $(eval $(python-package))
