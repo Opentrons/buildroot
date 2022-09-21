242fb11c6361f180cacfd6973931354f58fab18e
Seth Foster <seth@opentrons.com>
2022-09-21 14:45:45 -0400

    build: add sdk generation
    Buildroot SDKs are relocatable sysroots containing host tooling for
    cross compiling. With the BR2_PACKAGE_HOST_ENVIRONMENT_SETUP flag, it
    includes a script that will overwrite standard environment variables in
    a shell with those from the SDK. This can be used to provide the same
    cross-compiler environment that Buildroot uses outside buildroot itself,
    for application development.
    
    Closes RCORE-206

--- opentrons-build.sh
+++ opentrons-build.sh
@@ -59,6 +59,7 @@ case $# in
     0)
         docker run --env-file ./.env ${DOCKER_BIND} ${imgname} ot2_defconfig
         docker run --env-file ./.env ${DOCKER_BIND} ${imgname} all
+        docker run --env-file ./.env ${DOCKER_BIND} ${imgname} sdk
         ;;
     *)
         docker run --env-file ./.env ${heads} ${DOCKER_BIND} ${imgname} ${tail}
