df594b249402ac44b81fea95af5aacc3fc56c034
Ryan Howard <ryan.howard@opentrons.com>
2024-01-05 16:28:40 -0500

    upgrade nbformat to match other package upgrades

--- package/python-nbformat/python-nbformat.mk
+++ package/python-nbformat/python-nbformat.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-PYTHON_NBFORMAT_VERSION = 4.4.0
+PYTHON_NBFORMAT_VERSION = 5.9.2
 PYTHON_NBFORMAT_SOURCE = nbformat-$(PYTHON_NBFORMAT_VERSION).tar.gz
-PYTHON_NBFORMAT_SITE = https://files.pythonhosted.org/packages/6e/0e/160754f7ae3e984863f585a3743b0ed1702043a81245907c8fae2d537155
-PYTHON_NBFORMAT_SETUP_TYPE = distutils
+PYTHON_NBFORMAT_SITE = https://files.pythonhosted.org/packages/54/d8/31dceef56952da6ea2c43405a83c9759a22a86cb530197988cfa8599b178
+PYTHON_NBFORMAT_SETUP_TYPE = setuptools
 PYTHON_NBFORMAT_LICENSE = BSD-3-Clause
 
 $(eval $(python-package))
