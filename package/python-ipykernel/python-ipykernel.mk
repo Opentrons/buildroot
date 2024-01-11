c653c209d86d26eb2db4588558cd06efec553e7f
Ryan Howard <ryan.howard@opentrons.com>
2024-01-11 18:23:19 -0500

    add all the missing dependencies

--- package/python-ipykernel/python-ipykernel.mk
+++ package/python-ipykernel/python-ipykernel.mk
@@ -4,11 +4,11 @@
 #
 ################################################################################
 
-PYTHON_IPYKERNEL_VERSION = 6.25.2
+PYTHON_IPYKERNEL_VERSION = 6.20.2
 PYTHON_IPYKERNEL_SOURCE = ipykernel-$(PYTHON_IPYKERNEL_VERSION).tar.gz
-PYTHON_IPYKERNEL_SITE = https://files.pythonhosted.org/packages/8b/4a/3c1ba010e1517191eefb9f2fc556a3aed146b8c4e65708aed3ad17e803c5
+PYTHON_IPYKERNEL_SITE = https://files.pythonhosted.org/packages/87/c0/16e5656247203e7ae67e528f580561c9b54166145c10b46e3ad117984902
 PYTHON_IPYKERNEL_SETUP_TYPE = setuptools
 PYTHON_IPYKERNEL_LICENSE = BSD-3-Clause
-PYTHON_IPYKERNEL_DEPENDENCIES = python-ipython python-jupyter_client host-python-ipython host-python-jupyter_client python-stack-data host-python-backcall
+PYTHON_IPYKERNEL_DEPENDENCIES = python-comm python-debugpy python-ipython python-jupyter_client host-python-ipython host-python-jupyter_client python-stack-data host-python-backcall
 
 $(eval $(python-package))
--- package/python-jupyter_client/python-jupyter_client.mk
+++ package/python-jupyter_client/python-jupyter_client.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_JUPYTER_CLIENT_VERSION = 5.2.4
+PYTHON_JUPYTER_CLIENT_VERSION = 6.1.12
 PYTHON_JUPYTER_CLIENT_SOURCE = jupyter_client-$(PYTHON_JUPYTER_CLIENT_VERSION).tar.gz
-PYTHON_JUPYTER_CLIENT_SITE = https://files.pythonhosted.org/packages/e5/5c/f9b2182a445e3a6a8204daf04193154c474220f836dbcedc7d139276600e
+PYTHON_JUPYTER_CLIENT_SITE = https://files.pythonhosted.org/packages/de/05/6b1809dbe46e21c4018721c14a989a150ff73b4ecf631fe6e22d02cac579
 PYTHON_JUPYTER_CLIENT_SETUP_TYPE = setuptools
 PYTHON_JUPYTER_CLIENT_LICENSE = BSD-3-Clause
 
