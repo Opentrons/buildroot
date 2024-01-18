895c8ec1a250b148c27d886f91c14c37e5714211
Ryan Howard <ryan.howard@opentrons.com>
2024-01-18 11:12:35 -0500

    rip out some 'optional' dependencies remove the packages from buildroot, remove the dependencies and write some patches to edit the toml files so pip doesn't complain

--- package/python-ipykernel/python-ipykernel.mk
+++ package/python-ipykernel/python-ipykernel.mk
@@ -9,6 +9,6 @@ PYTHON_IPYKERNEL_SOURCE = ipykernel-$(PYTHON_IPYKERNEL_VERSION).tar.gz
 PYTHON_IPYKERNEL_SITE = https://files.pythonhosted.org/packages/87/c0/16e5656247203e7ae67e528f580561c9b54166145c10b46e3ad117984902
 PYTHON_IPYKERNEL_SETUP_TYPE = pep517
 PYTHON_IPYKERNEL_LICENSE = BSD-3-Clause
-PYTHON_IPYKERNEL_DEPENDENCIES = python-comm python-debugpy python-ipython python-jupyter_client host-python-ipython host-python-jupyter_client python-stack-data host-python-backcall host-python-hatchling host-python-hatch-vcs
+PYTHON_IPYKERNEL_DEPENDENCIES = python-comm python-ipython python-jupyter_client host-python-ipython host-python-jupyter_client python-stack-data host-python-backcall host-python-hatchling host-python-hatch-vcs
 
 $(eval $(python-package))
