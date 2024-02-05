ac912c048624b22f832aafd5574ffdc4def39900
Seth Foster <seth@opentrons.com>
2024-02-05 10:27:09 -0500

    package/python-sniffio: 1.3.0

--- package/python-sniffio/python-sniffio.mk
+++ package/python-sniffio/python-sniffio.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_SNIFFIO_VERSION = 1.2.0
+PYTHON_SNIFFIO_VERSION = 1.3.0
 PYTHON_SNIFFIO_SOURCE = sniffio-$(PYTHON_SNIFFIO_VERSION).tar.gz
-PYTHON_SNIFFIO_SITE = https://files.pythonhosted.org/packages/a6/ae/44ed7978bcb1f6337a3e2bef19c941de750d73243fc9389140d62853b686
+PYTHON_SNIFFIO_SITE = https://files.pythonhosted.org/packages/cd/50/d49c388cae4ec10e8109b1b833fd265511840706808576df3ada99ecb0ac
 PYTHON_SNIFFIO_SETUP_TYPE = setuptools
 PYTHON_SNIFFIO_LICENSE = Apache-2.0
 PYTHON_SNIFFIO_LICENSE_FILES = LICENSE
