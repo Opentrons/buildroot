7026c09839af722ae72fb484a3de0a065cc33f82
Seth Foster <seth@opentrons.com>
2024-02-05 10:25:32 -0500

    package/python-fastapi: 0.99.1

--- package/python-fastapi/python-fastapi.mk
+++ package/python-fastapi/python-fastapi.mk
@@ -4,11 +4,12 @@
 #
 ################################################################################
 
-PYTHON_FASTAPI_VERSION = 0.68.1
+PYTHON_FASTAPI_VERSION = 0.99.1
 PYTHON_FASTAPI_SOURCE = fastapi-$(PYTHON_FASTAPI_VERSION).tar.gz
-PYTHON_FASTAPI_SITE = https://files.pythonhosted.org/packages/e0/2a/03c3e1ede3aa83c45da1d156b0180288e573f5e2643ec4fd6d697cebebdf
-PYTHON_FASTAPI_SETUP_TYPE = distutils
+PYTHON_FASTAPI_SITE = https://files.pythonhosted.org/packages/22/93/42b9998a6e28eaee90724db439e27a66b7dc6a5b9e85499add1e042141e2
+PYTHON_FASTAPI_SETUP_TYPE = pep517
 PYTHON_FASTAPI_LICENSE = MIT
 PYTHON_FASTAPI_LICENSE_FILES = LICENSE
+PYTHON_FASTAPI_DEPENDENCIES = host-python-hatchling
 
 $(eval $(python-package))
