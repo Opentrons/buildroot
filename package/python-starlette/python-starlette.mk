d92c8147cc9451c307120b1b5c03823031320ee5
Seth Foster <seth@opentrons.com>
2024-02-05 10:27:25 -0500

    package/python-starlette: 0.27.0

--- package/python-starlette/python-starlette.mk
+++ package/python-starlette/python-starlette.mk
@@ -4,11 +4,12 @@
 #
 ################################################################################
 
-PYTHON_STARLETTE_VERSION = 0.14.2
+PYTHON_STARLETTE_VERSION = 0.27.0
 PYTHON_STARLETTE_SOURCE = starlette-$(PYTHON_STARLETTE_VERSION).tar.gz
-PYTHON_STARLETTE_SITE = https://files.pythonhosted.org/packages/3a/06/ded663a1ddea8b11a2027d88ff0e757f9cdb812310f18bee33ef7270112f
-PYTHON_STARLETTE_SETUP_TYPE = setuptools
+PYTHON_STARLETTE_SITE = https://files.pythonhosted.org/packages/06/68/559bed5484e746f1ab2ebbe22312f2c25ec62e4b534916d41a8c21147bf8
+PYTHON_STARLETTE_SETUP_TYPE = pep517
 PYTHON_STARLETTE_LICENSE = BSD-3-Clause
 PYTHON_STARLETTE_LICENSE_FILES = LICENSE.md
+PYTHON_STARLETTE_DEPENDENCIES = host-python-hatchling
 
 $(eval $(python-package))
