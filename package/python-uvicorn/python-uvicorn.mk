d1805f14abc9ff7229ffcb3675b002d465c66261
Seth Foster <seth@opentrons.com>
2024-02-05 10:27:41 -0500

    package/python-uvicorn: 0.27.0.post1

--- package/python-uvicorn/python-uvicorn.mk
+++ package/python-uvicorn/python-uvicorn.mk
@@ -4,9 +4,10 @@
 #
 ################################################################################
 
-PYTHON_UVICORN_VERSION = 0.14.0
+PYTHON_UVICORN_VERSION = 0.27.0.post1
 PYTHON_UVICORN_SOURCE = uvicorn-$(PYTHON_UVICORN_VERSION).tar.gz
-PYTHON_UVICORN_SITE = https://files.pythonhosted.org/packages/e9/9e/25d59f5043cf763833b2581c8027fa92342c4cf8ee523b498ecdf460c16d
-PYTHON_UVICORN_SETUP_TYPE = setuptools
+PYTHON_UVICORN_SITE = https://files.pythonhosted.org/packages/38/56/7bc5cf1d693d0c8e5d9dd66c29808691c17260b31346e4ddfbee26ba9bc2
+PYTHON_UVICORN_SETUP_TYPE = pep517
+PYTHON_UVICORN_DEPENDENCIES = host-python-hatchling
 
 $(eval $(python-package))
