9ff4736d85d1048f8425c404b89db06319f64ffb
Seth Foster <seth@opentrons.com>
2024-02-05 10:27:16 -0500

    package/python-sqlalchemy: 1.4.51

--- package/python-sqlalchemy/python-sqlalchemy.mk
+++ package/python-sqlalchemy/python-sqlalchemy.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_SQLALCHEMY_VERSION = 1.4.32
+PYTHON_SQLALCHEMY_VERSION = 1.4.51
 PYTHON_SQLALCHEMY_SOURCE = SQLAlchemy-$(PYTHON_SQLALCHEMY_VERSION).tar.gz
-PYTHON_SQLALCHEMY_SITE = https://files.pythonhosted.org/packages/7a/9f/ace7376a3ab45adf0f7169a5d8d60707c04b171b72a18bb23d505f83f362
+PYTHON_SQLALCHEMY_SITE = https://files.pythonhosted.org/packages/c8/56/5a8dcb01ef7b68904f2a3224343d4ab3674b5cc8f48f7cefb0701bc75ab8
 PYTHON_SQLALCHEMY_SETUP_TYPE = setuptools
 PYTHON_SQLALCHEMY_LICENSE = MIT
 PYTHON_SQLALCHEMY_LICENSE_FILES = LICENSE
