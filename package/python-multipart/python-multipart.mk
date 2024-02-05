342c52bffbc4f1e4469207c637c77e6a424b441f
Seth Foster <seth@opentrons.com>
2024-02-05 10:26:33 -0500

    package/python-multipart: 0.0.6

--- package/python-multipart/python-multipart.mk
+++ package/python-multipart/python-multipart.mk
@@ -4,11 +4,12 @@
 #
 ################################################################################
 
-PYTHON_MULTIPART_VERSION = 0.0.5
-PYTHON_MULTIPART_SOURCE = python-multipart-$(PYTHON_MULTIPART_VERSION).tar.gz
-PYTHON_MULTIPART_SITE = https://files.pythonhosted.org/packages/46/40/a933ac570bf7aad12a298fc53458115cc74053474a72fbb8201d7dc06d3d
-PYTHON_MULTIPART_SETUP_TYPE = setuptools
+PYTHON_MULTIPART_VERSION = 0.0.6
+PYTHON_MULTIPART_SOURCE = python_multipart-$(PYTHON_MULTIPART_VERSION).tar.gz
+PYTHON_MULTIPART_SITE = https://files.pythonhosted.org/packages/2d/23/abcfad10c3348cb6358400f8adbc21b523bbc6c954494fd0974428068672
+PYTHON_MULTIPART_SETUP_TYPE = pep517
 PYTHON_MULTIPART_LICENSE = Apache-2.0
 PYTHON_MULTIPART_LICENSE_FILES = LICENSE.txt
+PYTHON_MULTIPART_DEPENDENCIES = host-python-hatchling
 
 $(eval $(python-package))
