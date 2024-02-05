6d4889c576a162db2f37dd7b8556224d09b6670c
Seth Foster <seth@opentrons.com>
2024-02-05 10:26:49 -0500

    package/python-pydantic: 1.10.12

--- package/python-pydantic/python-pydantic.mk
+++ package/python-pydantic/python-pydantic.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_PYDANTIC_VERSION = 1.9.2
+PYTHON_PYDANTIC_VERSION = 1.10.12
 PYTHON_PYDANTIC_SOURCE = pydantic-$(PYTHON_PYDANTIC_VERSION).tar.gz
-PYTHON_PYDANTIC_SITE = https://files.pythonhosted.org/packages/fd/8f/3f7e88b507dbdfec8f1f914294aa8831edffb03d668799c65b4b46331c8a
+PYTHON_PYDANTIC_SITE = https://files.pythonhosted.org/packages/3b/9b/a7631bf35e55326fd74654fe6bd896478f47d65e97ca69e60ddb1b3823ee
 PYTHON_PYDANTIC_SETUP_TYPE = setuptools
 PYTHON_PYDANTIC_LICENSE = MIT
 PYTHON_PYDANTIC_LICENSE_FILES = LICENSE
