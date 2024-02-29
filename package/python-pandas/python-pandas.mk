8bd6b7bf67c1ad68ccd4b91dd4a00470cba78be2
Seth Foster <seth@opentrons.com>
2024-02-29 16:24:42 -0500

    try these

--- package/python-pandas/python-pandas.mk
+++ package/python-pandas/python-pandas.mk
@@ -7,7 +7,7 @@
 PYTHON_PANDAS_VERSION = 2.0.3
 PYTHON_PANDAS_SOURCE = pandas-$(PYTHON_PANDAS_VERSION).tar.gz
 PYTHON_PANDAS_SITE = https://files.pythonhosted.org/packages/b1/a7/824332581e258b5aa4f3763ecb2a797e5f9a54269044ba2e50ac19936b32
-PYTHON_PANDAS_DEPENDENCIES = host-python-numpy host-python-cython host-python-versioneer
+PYTHON_PANDAS_DEPENDENCIES = python-numpy host-python-numpy host-python-cython host-python-versioneer host-python-numpy
 PYTHON_PANDAS_SETUP_TYPE = setuptools
 PYTHON_PANDAS_LICENSE = BSD
 PYTHON_PANDAS_LICENSE_FILES = LICENSE
@@ -18,5 +18,7 @@ define PYTHON_PANDAS_REMOVE_TESTS
 endef
 PYTHON_PANDAS_POST_INSTALL_TARGET_HOOKS += PYTHON_PANDAS_REMOVE_TESTS
 endif
+
+PYTHON_PANDAS_ENV=SETUPTOOLS_BUILD_ARGS="--plat-name=linux-armv7l"
 
 $(eval $(python-package))
