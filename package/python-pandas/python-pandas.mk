af3592146e5c87db14618abb983a1779b0d0c8c2
Seth Foster <seth@opentrons.com>
2024-02-29 09:33:44 -0500

    that one's on me

--- package/python-pandas/python-pandas.mk
+++ package/python-pandas/python-pandas.mk
@@ -6,7 +6,7 @@
 
 PYTHON_PANDAS_VERSION = 2.0.3
 PYTHON_PANDAS_SOURCE = pandas-$(PYTHON_PANDAS_VERSION).tar.gz
-PYTHON_PANDAS_SITE = https://files.pythonhosted.org/packages/b1/a7/824332581e258b5aa4f3763ecb2a797e5f9a54269044ba2e50ac19936b32/pandas-2.0.3.tar.gzhttps://files.pythonhosted.org/packages/b1/a7/824332581e258b5aa4f3763ecb2a797e5f9a54269044ba2e50ac19936b32
+PYTHON_PANDAS_SITE = https://files.pythonhosted.org/packages/b1/a7/824332581e258b5aa4f3763ecb2a797e5f9a54269044ba2e50ac19936b32
 PYTHON_PANDAS_DEPENDENCIES = host-python-numpy host-python-cython host-python-versioneer
 PYTHON_PANDAS_SETUP_TYPE = setuptools
 PYTHON_PANDAS_LICENSE = BSD
