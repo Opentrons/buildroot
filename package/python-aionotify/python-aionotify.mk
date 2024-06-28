8be72a6cd4c2cd97a38bd3cfba91f0d13af37934
Seth Foster <seth@opentrons.com>
2024-06-28 10:59:16 -0400

    python/aionotify: 0.3.1
    Bump to match https://github.com/Opentrons/opentrons/pull/15546 for 3.11
    compatibility in the monorepo

--- package/python-aionotify/python-aionotify.mk
+++ package/python-aionotify/python-aionotify.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_AIONOTIFY_VERSION = 0.2.0
+PYTHON_AIONOTIFY_VERSION = 0.3.1
 PYTHON_AIONOTIFY_SOURCE = aionotify-$(PYTHON_AIONOTIFY_VERSION).tar.gz
-PYTHON_AIONOTIFY_SITE = https://files.pythonhosted.org/packages/57/c0/237d434870a024a16d89751ec9f4e5340a6d84d49ccb1ab738b3fdee907a
+PYTHON_AIONOTIFY_SITE = https://files.pythonhosted.org/packages/23/16/81a26a64d728e76eea073cd0316f3e8885cca312247a9ba9af64d7c47e64
 PYTHON_AIONOTIFY_LICENSE = BSD
 PYTHON_AIONOTIFY_LICENSE_FILES = LICENSE
 PYTHON_AIONOTIFY_SETUP_TYPE = setuptools
