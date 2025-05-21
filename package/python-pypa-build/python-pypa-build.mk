94b71662a3ba7ee37511832ee85bb97b9dbf983f
Seth Foster <seth@opentrons.com>
2025-05-21 16:54:05 -0400

    at this point we need tomli

--- package/python-pypa-build/python-pypa-build.mk
+++ package/python-pypa-build/python-pypa-build.mk
@@ -13,6 +13,7 @@ PYTHON_PYPA_BUILD_SETUP_TYPE = flit-bootstrap
 HOST_PYTHON_PYPA_BUILD_DEPENDENCIES = \
 	host-python-packaging \
 	host-python-pyproject-hooks \
+	host-python-tomli \
 
 
 $(eval $(host-python-package))
