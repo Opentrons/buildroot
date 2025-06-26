ddd49f523f65dbb121250d8f4b165615f65dd023
Caila Marashaj <caila.marashaj@opentrons.com>
2025-06-26 18:29:46 +0000

    fix urls

--- package/opentrons/opentrons-magnetic-module-firmware/opentrons-magnetic-module-firmware.mk
+++ package/opentrons/opentrons-magnetic-module-firmware/opentrons-magnetic-module-firmware.mk
@@ -6,7 +6,7 @@
 
 OPENTRONS_MAGNETIC_MODULE_FIRMWARE_VERSION=v2.0.1
 OPENTRONS_MAGNETIC_MODULE_FIRMWARE_SOURCE=mag-deck-arduino.ino.hex
-OPENTRONS_MAGNETIC_MODULE_FIRMWARE_SITE=https://opentrons-modules-builds.s3.us-east-2.amazonaws.com/magnetic-module/$(OPENTRONS_MAGNETIC_MODULE_FIRMWARE_VERSION)
+OPENTRONS_MAGNETIC_MODULE_FIRMWARE_SITE=https://opentrons-modules-builds.s3.us-east-2.amazonaws.com/modules-magdeck@v2.0.1-/magdeck/
 OPENTRONS_MAGNETIC_MODULE_FIRMWARE_LICENSE=Apache-2.0
 
 # not downloaded in a tarball so don't extract
