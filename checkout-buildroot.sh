d130738788ee30876aebe1b74e3cfd910975d90c
Seth Foster <seth@opentrons.com>
2025-10-31 12:44:21 -0400

    chore: store the ref in a file we can check
    This lets us have a single workflow that can build both the current
    setup and the old setup, which is pretty important to make sure that we
    don't kill the ability to build the old setup (which we will want since
    we will have other releases while developing this).

--- checkout-buildroot.sh
+++ checkout-buildroot.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env sh
 BUILDROOT_SIBLING_DIR="../buildroot-upstream"
-BUILDROOT_VERSION="2023.02.5"
+BUILDROOT_VERSION=$(cat ./buildroot-upstream-ref)
 
 if [ -e ${BUILDROOT_SIBLING_DIR} ] ; then
     echo "../buildroot-upstream exists, removing it"
     rm -rf ${BUILDROOT_SIBLING_DIR}
 fi
 
-echo "checking out ../buildroot-upstream"
+echo "checking out ../buildroot-upstream at ${BUILDROOT_VERSION}"
 git -C ../ clone https://github.com/buildroot/buildroot.git --branch=${BUILDROOT_VERSION} --depth=1 ./buildroot-upstream
