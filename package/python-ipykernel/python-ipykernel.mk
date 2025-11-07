97e71ec8c5ce9e53d04cf4de2aed2913329438d1
Seth Foster <seth@opentrons.com>
2025-11-07 13:31:31 -0500

    package/python-ipykernel: remove host jupyter
    ipykernel needs the rest of the jupyter stack to be present when it
    builds because it dynamically creates a json api spec for jupyter at
    build time. This is pretty reasonable when not cross-compiling (and when
    wanting to support an arbitrary version of jupyter-client) but needing
    the jupyter stack for the host requires adding host build support for a
    truly tremendous number of packages, and we only need to support exactly
    one jupyter version, so just inline it.
    
    We'll need to regenerate 0002-vendor-jupyter-api-for-6.1.12.patch
    whenever we update the version of jupyter we install.

--- package/python-ipykernel/python-ipykernel.mk
+++ package/python-ipykernel/python-ipykernel.mk
@@ -7,8 +7,22 @@
 PYTHON_IPYKERNEL_VERSION = 6.20.2
 PYTHON_IPYKERNEL_SOURCE = ipykernel-$(PYTHON_IPYKERNEL_VERSION).tar.gz
 PYTHON_IPYKERNEL_SITE = https://files.pythonhosted.org/packages/87/c0/16e5656247203e7ae67e528f580561c9b54166145c10b46e3ad117984902
-PYTHON_IPYKERNEL_SETUP_TYPE = pep517
+PYTHON_IPYKERNEL_SETUP_TYPE = hatch
 PYTHON_IPYKERNEL_LICENSE = BSD-3-Clause
-PYTHON_IPYKERNEL_DEPENDENCIES = python-comm python-ipython python-jupyter_client host-python-ipython host-python-jupyter_client python-stack-data host-python-backcall host-python-hatchling host-python-hatch-vcs
-
+PYTHON_IPYKERNEL_DEPENDENCIES = python-comm \
+								python-ipython \
+								python-jupyter_client \
+								python-traitlets \
+								python-tornado \
+								python-matplotlib-inline \
+								python-pyzmq \
+								python-psutil \
+								python-nest-asyncio \
+								python-packaging \
+								host-python-hatch-vcs
+define PYTHON_IPYKERNEL_INSTALL_LOGOS
+	cp $(PYTHON_IPYKERNEL_PKGDIR)/*.png $(@D)/data_kernelspec/
+	cp $(PYTHON_IPYKERNEL_PKGDIR)/*.svg $(@D)/data_kernelspec/
+endef
+PYTHON_IPYKERNEL_POST_PATCH_HOOKS += PYTHON_IPYKERNEL_INSTALL_LOGOS
 $(eval $(python-package))
