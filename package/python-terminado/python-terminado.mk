5d115b17e4ccd1f2538010ef146dedf03f68024f
Seth Foster <seth@opentrons.com>
2025-11-10 15:44:26 -0500

    package/python-terminado: bump to 0.18.1
    needed for jupyter updates

--- package/python-terminado/python-terminado.mk
+++ package/python-terminado/python-terminado.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-PYTHON_TERMINADO_VERSION = 0.8.1
+PYTHON_TERMINADO_VERSION = 0.18.1
 PYTHON_TERMINADO_SOURCE = terminado-$(PYTHON_TERMINADO_VERSION).tar.gz
-PYTHON_TERMINADO_SITE = https://files.pythonhosted.org/packages/67/84/ce0ebd0f60e1cbe040f8e065eef7063855d59d9cf5e6438b3f8439fc7e15
-PYTHON_TERMINADO_SETUP_TYPE = setuptools
+PYTHON_TERMINADO_SITE = https://files.pythonhosted.org/packages/8a/11/965c6fd8e5cc254f1fe142d547387da17a8ebfd75a3455f637c663fb38a0
+PYTHON_TERMINADO_SETUP_TYPE = hatch
 PYTHON_TERMINADO_LICENSE = BSD-3-Clause
 PYTHON_TERMINADO_LICENSE_FILES = LICENSE
 
