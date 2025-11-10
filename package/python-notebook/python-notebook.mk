22c9beabbec652598394d2f5fe5814d203310837
Seth Foster <seth@opentrons.com>
2025-11-10 15:45:50 -0500

    package/python-notebook: bump to 6.5.7
    Needed for python version and dep updates

--- package/python-notebook/python-notebook.mk
+++ package/python-notebook/python-notebook.mk
@@ -4,13 +4,25 @@
 #
 ################################################################################
 
-PYTHON_NOTEBOOK_VERSION = 6.0.3
+PYTHON_NOTEBOOK_VERSION = 6.5.7
 PYTHON_NOTEBOOK_SOURCE = notebook-$(PYTHON_NOTEBOOK_VERSION).tar.gz
-PYTHON_NOTEBOOK_SITE = https://files.pythonhosted.org/packages/a9/c8/77ab314f1a0102c50762efcc2b58be99780ddffb88bcd5820e2715e1799e
-PYTHON_NOTEBOOK_SETUP_TYPE = setuptools
+PYTHON_NOTEBOOK_SITE = https://files.pythonhosted.org/packages/05/bc/b025bac523640c13b0c1150466475eac3d79acbf187ef6c1967596c41c43
+PYTHON_NOTEBOOK_SETUP_TYPE = pep517
 PYTHON_NOTEBOOK_LICENSE = BSD-3-Clause
 
-PYTHON_NOTEBOOK_DEPENDENCIES = python-setuptools python-jinja2 python-tornado python-pyzmq python-ipython-genutils python-traitlets python-jupyter_core python-jupyter_client python-nbformat python-nbconvert python-ipykernel python-send2trash python-terminado python-prometheus_client
+PYTHON_NOTEBOOK_DEPENDENCIES = host-python-jupyter-packaging \
+								python-jinja2 python-tornado \
+								python-pyzmq python-ipython-genutils \
+								python-traitlets \
+								python-jupyter_core \
+								python-jupyter_client \
+								python-nbformat \
+								python-nbconvert \
+								python-ipykernel \
+								python-send2trash \
+								python-terminado \
+								python-prometheus_client \
+								python-nbclassic
 
 define PYTHON_NOTEBOOK_USERS
 	jupyter -1 jupyter -1 * - - -
