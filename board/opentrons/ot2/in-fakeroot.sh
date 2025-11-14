ba4a8ae110763c5834ee641183629337f7178d77
Seth Foster <seth@opentrons.com>
2025-11-14 16:57:44 -0500

    board/opentrons/ot2: remove pycaches
    These take up space and we can't afford it.

--- board/opentrons/ot2/in-fakeroot.sh
+++ board/opentrons/ot2/in-fakeroot.sh
@@ -1,7 +1,10 @@
+set -ev
+# remove __pycaches__ since they just take space
+find ${TARGET_DIR}/usr/lib/python3.12/site-packages -name __pycache__ -delete
+
 # Remove /var from rootfs because it will be in its own partition
 GENIMAGE_TMP="${BUILD_DIR}/genimage.tmp"
 mkdir -p "${GENIMAGE_TMP}"
-set -ev
 rm -rf "${GENIMAGE_TMP}/var"
 mv "${TARGET_DIR}/var" "${GENIMAGE_TMP}/"
 mkdir -p "${TARGET_DIR}/var"
